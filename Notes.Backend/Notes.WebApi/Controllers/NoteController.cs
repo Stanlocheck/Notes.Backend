84bad9d58478367f5783b4bcb4ee13410b0ace78
Алексей Шелухин <sheluhinalekseypavlovich@gmail.com>
2024-01-18 17:40:20 +0300

    Добавил автодокументирование Swagger

--- Notes.Backend/Notes.WebApi/Controllers/NoteController.cs
+++ Notes.Backend/Notes.WebApi/Controllers/NoteController.cs
@@ -11,6 +11,7 @@ using Microsoft.AspNetCore.Authorization;
 
 namespace Notes.WebApi.Controllers
 {
+    [Produces("application/json")]
     [Route("api/[controller]")]
     public class NoteController : BaseController
     {
@@ -18,8 +19,21 @@ namespace Notes.WebApi.Controllers
 
         public NoteController(IMapper mapper) => _mapper = mapper;
 
+        /// <summary>
+        /// Gets the list of notes
+        /// </summary>
+        /// <remarks>
+        /// Sample request:
+        /// GET /note
+        /// </remarks>
+        /// <returns>Returns NoteListVm</returns>
+        /// <response code="200">Success</response>
+        /// <response code="401">If the user is unauthorized</response>
+
         [HttpGet]
         [Authorize]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
         public async Task<ActionResult<NoteListVm>> GetAll()
         {
             var query = new GetNoteListQuery
@@ -30,8 +44,22 @@ namespace Notes.WebApi.Controllers
             return Ok(vm);
         }
 
+        /// <summary>
+        /// Gets the note by id
+        /// </summary>
+        /// <remarks>
+        /// Sample request:
+        /// GET /note/FA01E371-BB74-4AE5-8FDE-ECA5AD58FEB6
+        /// </remarks>
+        /// <param name="id">Note id (guid)</param>
+        /// <returns>Returns NoteDetailsVm</returns>
+        /// <response code="200">Success</response>
+        /// <response code="401">If the user is unauthorized</response>
+
         [HttpGet("{id}")]
         [Authorize]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
         public async Task<ActionResult<NoteDetailsVm>> Get(Guid id)
         {
             var query = new GetNoteDetailsQuery
@@ -43,8 +71,26 @@ namespace Notes.WebApi.Controllers
             return Ok(vw);
         }
 
+        /// <summary>
+        /// Gets the note
+        /// </summary>
+        /// <remarks>
+        /// Sample request:
+        /// POST /note
+        /// {
+        ///     title: "note title",
+        ///     details: "note details"
+        /// }
+        /// </remarks> 
+        /// <param name="createNoteDto">CreateNoteDto object</param>
+        /// <returns>Returns id (guid)</returns>
+        /// <response code="200">Success</response>
+        /// <response code="401">If the user is unauthorized</response>
+
         [HttpPost]
         [Authorize]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
         public async Task<ActionResult<Guid>> Create([FromBody] CreateNoteDto createNoteDto)
         {
             var command = _mapper.Map<CreateNoteCommand>(createNoteDto);
@@ -53,8 +99,25 @@ namespace Notes.WebApi.Controllers
             return Ok(noteId);
         }
 
+        /// <summary>
+        /// Updates the note
+        /// </summary>
+        /// <remarks>
+        /// Sample request:
+        /// PUT /note
+        /// {
+        ///     title: "update note title"
+        /// }
+        /// </remarks>
+        /// <param name="updateNoteDto">UpdateNoteDto object</param>
+        /// <returns>Returns NoContent</returns>
+        /// <response code="204">Success</response>
+        /// <response code="401">If the user is unauthorized</response>
+
         [HttpPut]
         [Authorize]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
         public async Task<IActionResult> Update([FromBody] UpdateNoteDto updateNoteDto)
         {
             var command = _mapper.Map<UpdateNoteCommand>(updateNoteDto);
@@ -63,8 +126,22 @@ namespace Notes.WebApi.Controllers
             return NoContent();
         }
 
+        /// <summary>
+        /// Deletes the note by id
+        /// </summary>
+        /// <remarks>
+        /// Sample request:
+        /// DELETE /note/B79BA68B-98DA-48EA-998D-1A5C93CF0F70
+        /// </remarks>
+        /// <param name="id">Id of the note (guid)</param>
+        /// <returns>Returns NoContent</returns>
+        /// <response code="204">Success</response>
+        /// <response code="401">If the user is unauthorized</response>
+
         [HttpDelete("{id}")]
         [Authorize]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
         public async Task<IActionResult> Delete(Guid id)
         {
             var command = new DeleteNoteCommand
